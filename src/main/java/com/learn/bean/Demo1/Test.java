948be0d67d39ad60fec6b1c55d3584745e7da3ad
Yummyxl <2548341581@qq.com>
2020-03-04 14:43:46 +0800

    ioc 对象创建步骤

--- src/main/java/com/learn/bean/Demo1/Test.java
+++ src/main/java/com/learn/bean/Demo1/Test.java
@@ -10,6 +10,13 @@ import org.springframework.core.io.ClassPathResource;
  *
  * @Author: Yummyxl
  * @Date: 2020/3/2
+ *
+ * 1 spring的bean实际上是缓存在ConcurrentHashMap中
+ * 2 在创建bean之前首选需要将该bean的创建标示定好，标示该bean已经或是正在被创建，为了增加缓存效率
+ * 3 根据bean的scope属性来确定bean是Singleton还是prototype
+ * 4 通过反射来创建bean的实例，在创建之前首先检查访问修饰符，如果不是public的，则调用setAccessible(true)来突破java先知来创建bean实例
+ * 5 接下来寻找bean的属性值完成属性的注入
+ * 6 将创建出的Singleton对象放入缓存中
  */
 
 public class Test {
@@ -24,14 +31,16 @@ public class Test {
 
         xmlBeanDefinitionReader.loadBeanDefinitions(classPathResource);
 
-        Person person1 = defaultListableBeanFactory.getBean(Person.class);
+//        Person person1 = defaultListableBeanFactory.getBean(Person.class);
+
+        Person person = (Person) defaultListableBeanFactory.getBean("person");
 
         Person person2 = defaultListableBeanFactory.getBean(Person.class);
 
-        person1.setName("1");
-
-        System.out.println(person1.say());
-
-        System.out.println(person1 == person2);
+//        person1.setName("1");
+//
+//        System.out.println(person1.say());
+//
+//        System.out.println(person1 == person2);
     }
 }
